e6c0ba342f7c7f557f9ed710935681fabd37e024
Rudy Flores <38470001+rudypflores@users.noreply.github.com>
2019-01-04 14:00:59 -0600

    fix for education tabs

--- js/logic.js
+++ js/logic.js
@@ -1,4 +1,6 @@
 //function checks if the user clicked the section yet
+let active = false;
+
 const isClicked = (display) => {
 	if(display === 'block') {
 		return true;
@@ -20,10 +22,27 @@ const openSection = (name, icon) => {
 		section.style.display = 'block';
 		arrow.style.transition = 'transform 0.2s ease-in 0s';
 		arrow.style.transform = 'rotate(90deg)';
+		active = true;
 	} else {
 		arrow.style.transition = 'transform 0.2s linear 0s';
 		arrow.style.transform = 'rotate(0deg)';
 		section.style.display = 'none';
-		
+		active = false;
 	}
+};
+
+//Hover animation
+/*Made the decision of making this through JS instead of using
+ css :hover because it has its limitations when triggering the
+ openSection event handler*/
+const lightUp = (name) => {
+	let tab = document.getElementById(name);
+	tab.style.opacity = '1';
+	tab.onmouseout = () => {
+		if(active) {
+			tab.style.opacity = '1';
+		} else {
+			tab.style.opacity = '0.7';
+		}
+	};
 };
