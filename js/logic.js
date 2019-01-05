c6d88ccb4d1c5ee787c1e6abc6b639702b43f92e
Rudy Flores <38470001+rudypflores@users.noreply.github.com>
2019-01-04 18:20:06 -0600

    transition update

--- js/logic.js
+++ js/logic.js
@@ -46,3 +46,40 @@ const lightUp = (name) => {
 		}
 	};
 };
+
+//intro animation
+window.onload = () => {
+	//Access the parent class
+	let master = document.getElementById('hero-text').children;
+
+	console.log(master);
+
+	//Store the properties we will modify from the children
+	let greet = master[0].style;
+	let title = master[2].style;
+	let sub = master[4].style;
+	let about = master[6].style;
+
+	//fade in animation
+	greet.transition = 'opacity 1s ease-in 0s';
+	title.transition = 'opacity 1s ease-in 1.3s';
+	sub.transition = 'opacity 1s ease-in 3s';
+	about.transition = 'opacity 1s ease-in 5s';
+
+	greet.opacity = '1';
+	title.opacity = '1';
+	sub.opacity = '1';
+	about.opacity = '1';
+
+	//Slide in top bar
+	let bar = document.getElementById('bar').style;
+	let items = document.getElementById('items').style;
+
+	bar.transition = 'width .5s ease-in-out 0s, height .5s ease-in-out .5s';
+	items.transition = 'opacity 1s ease-in 1s';
+
+	bar.width = '100%';
+	bar.height = '75px';
+	items.opacity = '1';
+
+};
